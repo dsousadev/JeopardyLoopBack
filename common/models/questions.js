76ad2d33d6a2c1c09d43f4cca99bc93ae3b45b3d
David de Sousa <admin@anatta.local>
2017-10-12 20:31:01 -0700

    Implements random endpoint to return random question

--- common/models/questions.js
+++ common/models/questions.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Questions) {
+  Questions.random = cb => {
+    Questions.getDataSource().connector.connect((err, db) => {
+      const collection = db.collection('questions');
+      collection.aggregate({$sample: {size: 1}}, (err, data) => {
+        if (err) return cb(err);
+        return cb(null, data);
+      });
+    });
+  };
 
+  Questions.remoteMethod('random', {
+    http: {
+      path: '/random',
+      verb: 'get',
+    },
+    description: 'Gets one random question',
+    returns: {
+      arg: 'questions',
+      type: 'json',
+    },
+  });
 };
